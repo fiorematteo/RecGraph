b21ce541adcfd63290bcc79228c9ad01c98b7292
matteo <fiorematteo2002@gmail.com>
2023-04-01 16:57:43 +0200

    Added reverse handle_map and fixed bug in distance function

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ use recgraph::qgrams;
 use recgraph::score_matrix;
 use recgraph::sequences;
 use recgraph::utils;
-use std::collections::HashMap;
 
 use std::time::SystemTime;
 
@@ -50,16 +49,18 @@ fn main() {
 
     //get handle position for output
     let hofp_forward = utils::create_handle_pos_in_lnz(&graph_struct.nwp, &graph_path, false);
-    let mut hofp_reverse = HashMap::new();
+    let hofp_reverse = utils::create_handle_pos_in_lnz(&graph_struct.nwp, &graph_path, true);
 
     //optimize graph
-    let mut graph_optimizer = qgrams::get_optimizer(&graph_struct, &hofp_forward, qgrams_len);
+    let mut graph_optimizer =
+        qgrams::get_optimizer(&graph_struct, &hofp_forward, &hofp_reverse, qgrams_len);
 
     match align_mode {
         //global alignment
         0 => {
             for (i, seq) in sequences.iter().enumerate() {
-                let (graph_struct, hofp_forward) = graph_optimizer.optimize_graph(seq);
+                let (graph_struct, hofp_forward, hofp_reverse) =
+                    graph_optimizer.optimize_graph(seq);
                 let r_values = utils::set_r_values(
                     &graph_struct.nwp,
                     &graph_struct.pred_hash,
@@ -91,10 +92,10 @@ fn main() {
                     )
                 };
                 if amb_strand && alignment.0 < 0 {
-                    if hofp_reverse.is_empty() {
-                        hofp_reverse =
-                            utils::create_handle_pos_in_lnz(&graph_struct.nwp, &graph_path, true);
-                    }
+                    // if hofp_reverse.is_empty() {
+                    //     hofp_reverse =
+                    //         utils::create_handle_pos_in_lnz(&graph_struct.nwp, &graph_path, true);
+                    // }
                     let rev_seq = sequences::rev_and_compl(seq);
                     let rev_alignment = global_abpoa::exec(
                         &rev_seq,
@@ -118,8 +119,9 @@ fn main() {
         //local alignment
         1 => {
             for (i, seq) in sequences.iter().enumerate() {
-                let (graph_struct, hofp_forward) = graph_optimizer.optimize_graph(seq);
-                let alignment = if is_x86_feature_detected!("avx2") && false {
+                let (graph_struct, hofp_forward, hofp_reverse) =
+                    graph_optimizer.optimize_graph(seq);
+                let alignment = if is_x86_feature_detected!("avx2") {
                     unsafe {
                         let temp_score = local_poa::exec_simd(
                             seq,
@@ -142,10 +144,10 @@ fn main() {
                     )
                 };
                 if amb_strand {
-                    if hofp_reverse.is_empty() {
-                        hofp_reverse =
-                            utils::create_handle_pos_in_lnz(&graph_struct.nwp, &graph_path, true);
-                    }
+                    // if hofp_reverse.is_empty() {
+                    //     hofp_reverse =
+                    //         utils::create_handle_pos_in_lnz(&graph_struct.nwp, &graph_path, true);
+                    // }
                     let rev_seq = sequences::rev_and_compl(seq);
                     let alignment_rev = if is_x86_feature_detected!("avx2") {
                         unsafe {
@@ -184,7 +186,8 @@ fn main() {
             let (g_open, g_ext) = args_parser::get_gap_open_gap_ext();
 
             for (i, seq) in sequences.iter().enumerate() {
-                let (graph_struct, hofp_forward) = graph_optimizer.optimize_graph(seq);
+                let (graph_struct, hofp_forward, hofp_reverse) =
+                    graph_optimizer.optimize_graph(seq);
                 let bases_to_add = (b + f * seq.len() as f32) as usize;
                 let alignment = gap_global_abpoa::exec(
                     seq,
@@ -199,10 +202,10 @@ fn main() {
                 );
 
                 if amb_strand && alignment.0 < 0 {
-                    if hofp_reverse.is_empty() {
-                        hofp_reverse =
-                            utils::create_handle_pos_in_lnz(&graph_struct.nwp, &graph_path, true);
-                    }
+                    // if hofp_reverse.is_empty() {
+                    //     hofp_reverse =
+                    //         utils::create_handle_pos_in_lnz(&graph_struct.nwp, &graph_path, true);
+                    // }
                     let rev_seq = sequences::rev_and_compl(seq);
                     let rev_alignment = gap_global_abpoa::exec(
                         &rev_seq,
@@ -229,7 +232,8 @@ fn main() {
         3 => {
             let (g_open, g_ext) = args_parser::get_gap_open_gap_ext();
             for (i, seq) in sequences.iter().enumerate() {
-                let (graph_struct, hofp_forward) = graph_optimizer.optimize_graph(seq);
+                let (graph_struct, hofp_forward, hofp_reverse) =
+                    graph_optimizer.optimize_graph(seq);
                 let alignment = gap_local_poa::exec(
                     seq,
                     (&seq_names[i], i + 1),
@@ -241,10 +245,10 @@ fn main() {
                     &hofp_forward,
                 );
                 if amb_strand {
-                    if hofp_reverse.is_empty() {
-                        hofp_reverse =
-                            utils::create_handle_pos_in_lnz(&graph_struct.nwp, &graph_path, true);
-                    }
+                    // if hofp_reverse.is_empty() {
+                    //     hofp_reverse =
+                    //         utils::create_handle_pos_in_lnz(&graph_struct.nwp, &graph_path, true);
+                    // }
                     let rev_seq = sequences::rev_and_compl(seq);
                     let rev_alignment = gap_local_poa::exec(
                         &rev_seq,
--- src/qgrams.rs
+++ src/qgrams.rs
@@ -2,7 +2,7 @@ use crate::graph::LnzGraph;
 use bit_vec::BitVec;
 use std::{
     cell::RefCell,
-    collections::{HashMap, VecDeque},
+    collections::{HashMap, HashSet, VecDeque},
 };
 
 #[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
@@ -15,8 +15,6 @@ struct Bound {
     start: GramPoint,
     end: GramPoint,
     distance: usize,
-    begin_gram: String,
-    end_gram: String,
     read_offset: usize,
 }
 type GraphIndex = HashMap<String, Vec<GramPoint>>;
@@ -81,6 +79,7 @@ impl<'a> DistanceMap<'a> {
         for i in 0..graph.len() {
             cached_distance[i][i] = Some(0);
         }
+
         Self {
             graph,
             cached_distance: RefCell::new(cached_distance),
@@ -92,6 +91,8 @@ impl<'a> DistanceMap<'a> {
         if let Some(distance) = cache[from][to] {
             return distance;
         }
+
+        let mut visited = HashSet::new();
         let mut queue = VecDeque::new();
         queue.push_back(to);
         while let Some(node) = queue.pop_front() {
@@ -101,10 +102,13 @@ impl<'a> DistanceMap<'a> {
             for pred in self.graph.get_predecessors(node) {
                 if cache[pred][to].is_none() {
                     cache[pred][to] = Some(cache[node][to].unwrap() + 1);
+                }
+                if visited.insert(pred) {
                     queue.push_back(pred);
                 }
             }
         }
+        cache[from][to] = Some(usize::MAX);
         usize::MAX
     }
 }
@@ -112,16 +116,22 @@ impl<'a> DistanceMap<'a> {
 struct GraphOptimizer<'a> {
     graph: &'a LnzGraph,
     q: usize,
-    cache: HashMap<String, (Bound, HandlePos)>,
+    cache: HashMap<String, (Bound, HandlePos, HandlePos)>,
     distance: DistanceMap<'a>,
     graph_qgrams: GraphIndex,
     handle_pos: &'a HandlePos,
+    reverse_handle_pos: &'a HandlePos,
 }
 
 type HandlePos = HashMap<usize, String>;
 
 impl<'a> GraphOptimizer<'a> {
-    fn new(graph: &'a LnzGraph, handle_pos: &'a HandlePos, q: usize) -> Self {
+    fn new(
+        graph: &'a LnzGraph,
+        handle_pos: &'a HandlePos,
+        reverse_handle_pos: &'a HandlePos,
+        q: usize,
+    ) -> Self {
         let distance = DistanceMap::new(graph);
         let qgrams = graph.find_all_qgrams(q);
         assert!(qgrams.iter().all(|qgram| qgram.1.len() == q));
@@ -140,36 +150,48 @@ impl<'a> GraphOptimizer<'a> {
             distance,
             graph_qgrams,
             handle_pos,
+            reverse_handle_pos,
         }
     }
 
-    fn cut_graph(&mut self, bound: &Bound, read_len: usize) -> (LnzGraph, HandlePos) {
+    fn cut_graph(&mut self, bound: &Bound, read_len: usize) -> (LnzGraph, HandlePos, HandlePos) {
         let node_before = bound.read_offset;
 
         let starts: Vec<_> = (0..self.graph.len())
-            .filter(|&node| self.distance.get(bound.start.start, node) <= node_before)
+            .filter(|&node| self.distance.get(node, bound.start.start) <= node_before)
             .collect();
 
         let node_after = read_len - bound.read_offset + self.q;
         let ends: Vec<_> = (0..self.graph.len())
-            .filter(|&node| self.distance.get(node, bound.end.end) <= node_after)
+            .filter(|&node| self.distance.get(bound.end.end, node) <= node_after)
             .collect();
 
         self.cut_graph_exact(&starts, &ends)
     }
 
-    fn cut_graph_exact(&mut self, start: &[usize], end: &[usize]) -> (LnzGraph, HandlePos) {
-        let reachable_nodes = (0..self.graph.len())
-            .filter(|&node| {
-                end.iter()
-                    .any(|&end| self.distance.get(node, end) != usize::MAX)
-            })
+    fn cut_graph_exact(
+        &mut self,
+        start: &[usize],
+        end: &[usize],
+    ) -> (LnzGraph, HandlePos, HandlePos) {
+        let mut reachable_nodes = (0..self.graph.len())
             .filter(|&node| {
                 start
                     .iter()
                     .any(|&start| self.distance.get(start, node) != usize::MAX)
             })
+            .filter(|&node| {
+                end.iter()
+                    .any(|&end| self.distance.get(node, end) != usize::MAX)
+            })
             .collect::<Vec<_>>();
+
+        if reachable_nodes[0] != 0 {
+            reachable_nodes.insert(0, 0);
+        }
+        if reachable_nodes[reachable_nodes.len() - 1] != self.graph.len() - 1 {
+            reachable_nodes.push(self.graph.len() - 1);
+        }
 
         let lnz = reachable_nodes
             .iter()
@@ -182,26 +204,40 @@ impl<'a> GraphOptimizer<'a> {
             .enumerate()
             .for_each(|(i, &node)| nwp.set(i, self.graph.nwp[node]));
 
-        let pred_hash: HashMap<_, _> = reachable_nodes
+        let mut pred_hash: HashMap<_, _> = reachable_nodes
             .iter()
             .enumerate()
             .filter(|(_, node)| self.graph.nwp[**node])
             .map(|(i, node)| {
-                (
-                    i,
-                    self.graph.pred_hash[node]
-                        .iter()
-                        .filter(|&&pred| reachable_nodes.contains(&pred))
-                        .map(|&pred| reachable_nodes.iter().position(|&x| x == pred).unwrap())
-                        .collect::<Vec<_>>(),
-                )
+                let predecessors = self.graph.pred_hash[node]
+                    .iter()
+                    .filter(|pred| reachable_nodes.contains(pred))
+                    .map(|pred| reachable_nodes.iter().position(|x| x == pred).unwrap())
+                    .collect::<Vec<_>>();
+                (i, predecessors)
             })
             .collect();
 
-        let new_handle_pos: HandlePos = reachable_nodes
+        let mut last_nodes: Vec<_> = reachable_nodes
             .iter()
             .enumerate()
-            .map(|(i, node)| (i, self.handle_pos[node].clone()))
+            .filter(|(_, node)| pred_hash.values().all(|v| !v.contains(node)))
+            .map(|(i, _)| i)
+            .collect();
+
+        let last_preds = pred_hash.entry(reachable_nodes.len() - 1).or_insert(vec![]);
+        last_preds.append(&mut last_nodes);
+
+        let new_handle_pos = reachable_nodes
+            .iter()
+            .enumerate()
+            .filter_map(|(i, node)| Some((i, self.handle_pos.get(node)?.clone())))
+            .collect();
+
+        let new_reverse_handle_pos = reachable_nodes
+            .iter()
+            .enumerate()
+            .filter_map(|(i, node)| Some((i, self.reverse_handle_pos.get(node)?.clone())))
             .collect();
 
         (
@@ -211,6 +247,7 @@ impl<'a> GraphOptimizer<'a> {
                 pred_hash,
             },
             new_handle_pos,
+            new_reverse_handle_pos,
         )
     }
 
@@ -235,8 +272,6 @@ impl<'a> GraphOptimizer<'a> {
                                 start: begin_id,
                                 end: end_id,
                                 distance,
-                                begin_gram: begin_gram.to_string(),
-                                end_gram: end_gram.to_string(),
                                 read_offset: i,
                             });
                         }
@@ -255,51 +290,77 @@ impl<'a> GraphOptimizer<'a> {
 pub fn get_optimizer<'a>(
     graph: &'a LnzGraph,
     handle_pos: &'a HandlePos,
+    reverse_handle_pos: &'a HandlePos,
     q: usize,
 ) -> Box<dyn Optimizer + 'a> {
     if q == 0 {
-        Box::new(PassThrough::new(graph, handle_pos))
+        Box::new(PassThrough::new(graph, handle_pos, reverse_handle_pos))
     } else {
-        Box::new(GraphOptimizer::new(graph, handle_pos, q))
+        Box::new(GraphOptimizer::new(
+            graph,
+            handle_pos,
+            reverse_handle_pos,
+            q,
+        ))
     }
 }
 
 struct PassThrough<'a> {
     graph: &'a LnzGraph,
     handle_pos: &'a HandlePos,
+    reverse_handle_pos: &'a HandlePos,
 }
 
 impl<'a> PassThrough<'a> {
-    fn new(graph: &'a LnzGraph, handle_pos: &'a HandlePos) -> Self {
-        Self { graph, handle_pos }
+    fn new(
+        graph: &'a LnzGraph,
+        handle_pos: &'a HandlePos,
+        reverse_handle_pos: &'a HandlePos,
+    ) -> Self {
+        Self {
+            graph,
+            handle_pos,
+            reverse_handle_pos,
+        }
     }
 }
 
 pub trait Optimizer {
-    fn optimize_graph(&mut self, read: &[char]) -> (LnzGraph, HandlePos);
+    fn optimize_graph(&mut self, read: &[char]) -> (LnzGraph, HandlePos, HandlePos);
 }
 
 impl<'a> Optimizer for PassThrough<'a> {
-    fn optimize_graph(&mut self, _read: &[char]) -> (LnzGraph, HandlePos) {
-        (self.graph.clone(), self.handle_pos.clone())
+    fn optimize_graph(&mut self, _read: &[char]) -> (LnzGraph, HandlePos, HandlePos) {
+        (
+            self.graph.clone(),
+            self.handle_pos.clone(),
+            self.reverse_handle_pos.clone(),
+        )
     }
 }
 
 impl<'a> Optimizer for GraphOptimizer<'a> {
-    fn optimize_graph(&mut self, read: &[char]) -> (LnzGraph, HandlePos) {
+    fn optimize_graph(&mut self, read: &[char]) -> (LnzGraph, HandlePos, HandlePos) {
         let read: String = read.iter().collect();
-        if let Some((bound, handle_pos)) = self.cache.get(&read).cloned() {
-            let (graph, _) = self.cut_graph(&bound, read.len());
+        if let Some((bound, handle_pos, reverse_handle_pos)) = self.cache.get(&read).cloned() {
+            let (graph, _, _) = self.cut_graph(&bound, read.len());
             println!("graph went from {} to {}", self.graph.len(), graph.len());
-            return (graph, handle_pos);
+            return (graph, handle_pos, reverse_handle_pos);
         }
         if let Some(bound) = self.find_best_bound(&read) {
-            let (graph, handle_pos) = self.cut_graph(&bound, read.len());
-            self.cache.insert(read, (bound, handle_pos.clone()));
+            let (graph, handle_pos, reverse_handle_pos) = self.cut_graph(&bound, read.len());
+            self.cache.insert(
+                read,
+                (bound, handle_pos.clone(), reverse_handle_pos.clone()),
+            );
             println!("graph went from {} to {}", self.graph.len(), graph.len());
-            return (graph, handle_pos);
+            return (graph, handle_pos, reverse_handle_pos);
         }
         println!("graph is the same");
-        (self.graph.clone(), self.handle_pos.clone())
+        (
+            self.graph.clone(),
+            self.handle_pos.clone(),
+            self.reverse_handle_pos.clone(),
+        )
     }
 }
